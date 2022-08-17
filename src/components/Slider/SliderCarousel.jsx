74da79a5a6f3061803346d1c1fd239c620c7ca76
Eltac E. Malikmammadov <Eltacem@code.local>
2022-08-17 18:36:12 +0400

    com-5

--- src/components/Slider/SliderCarousel.jsx
+++ src/components/Slider/SliderCarousel.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Slider from "react-slick";
 import './Slider.css'
+import axios from 'axios'
 
 function SliderCarousel() {
+
+  const [sld,setSld] = useState([])
+
+  useEffect(()=>
+    {
+      axios.get("http://ejtacmalik-001-site1.btempurl.com/api/Homes/slider")
+      .then(resp=> setSld(resp.data))
+    },[])
+
+
+    console.log(sld);
+    
+
   const settings = {
     dots: true,
     autoplay: true,
@@ -19,15 +33,16 @@ function SliderCarousel() {
   return (
     <>
       <Slider {...settings}>
+      {
+        sld&&sld.map(e=>
+          {
+            return(
         <div className="slider-item">
-            <img src="https://cdn.shopify.com/s/files/1/0039/3740/2989/files/Slider-2_f7becb67-5722-4f24-810d-d8ad5fa7483e.jpg?v=1559368812" alt=""/>
+            <img src={`http://ejtacmalik-001-site1.btempurl.com/sliders/${e.image}`} alt=""/>
         </div>
-        <div className="slider-item q2">
-            <img src="https://images.pexels.com/photos/125779/pexels-photo-125779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
-        </div>
-        <div className="slider-item q3">
-            <img src="https://images.pexels.com/photos/2113994/pexels-photo-2113994.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt=""/>
-        </div>
+            )
+          })
+      }
       </Slider>
     </>
   );
