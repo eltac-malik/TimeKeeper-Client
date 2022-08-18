415b4c31f78bb4511a883b33412afd3b6f95c541
Eltac E. Malikmammadov <Eltacem@code.local>
2022-08-18 15:49:39 +0400

    com-6-home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,8 @@ import "../assets/css/Home.css"
 import SliderCarousel from 'components/Slider/SliderCarousel'
 import Support from 'components/Support/Support'
 import Famous from 'components/FamousCardCarousel/index'
+import Gift from 'components/Gift/Gift'
+import Watch from 'components/Watch/Watch';
 
 function Home() {
     return (
@@ -13,6 +15,12 @@ function Home() {
         </div>
         <div className="cont">
             <Famous/>
+        </div>
+        <div className="cont">
+            <Gift/>
+        </div>
+        <div className="cont">
+            <Watch/>
         </div>
         </div>
     )
